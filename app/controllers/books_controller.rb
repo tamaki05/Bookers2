054d4acd8713129697470293db20e0f87b41d152
EC2 Default User <ec2-user@ip-172-31-56-226.ec2.internal>
2022-06-19 14:47:48 +0000

    [Update]投稿機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,15 +6,26 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+     if @book.save
+       redirect_to books_path
+     else
+       render :new
+     end
   end
 
   def index
-    @book = Book.all
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
+  end
+
+  def edit
     @book = Book.find(params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
   resources :users, only: [:show, :edit, :update]
   
   get "homes/about" => "homes#about", as: "about"
